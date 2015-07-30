50920d80f8f5dc9b3f10ce4177ab25515d4fe653
wsw621012 <lucifer.swwang@gmail.com>
2015-07-30 10:41:20 +0800

    Security_Test

---
+++ SecurityTest/ProtecteDataTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Security.Cryptography;
+
+namespace ProtecteDataTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "Tr3ndM!cr0";
+
+            string entry = "File Reputation Service";
+
+            var encData = ProtectedData.Protect(GetBytes(pass), GetBytes(entry), DataProtectionScope.LocalMachine);
+
+            var encString = Convert.ToBase64String(encData);
+
+            Console.WriteLine("encrypted string: {0}", encString);
+
+            var decData = Convert.FromBase64String(encString);
+
+            var inverse = ProtectedData.Unprotect(decData, GetBytes(entry), DataProtectionScope.LocalMachine);
+
+            Console.WriteLine("inverse: {0}", GetString(inverse));
+
+            Console.ReadKey();
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+    }
+}
